e2c4c9c90347fc992045f561b6bbe6bbaa39cb1f
DevHyung <khuphj@gmail.com>
2018-07-18 02:00:30 +0900

    Env Commit

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ if __name__=="__main__":
     #print(html.encoding) # ISO-8859-1 인코딩나와서
     html.encoding = 'euc-kr'  # 한글 인코딩으로 변환
     bs4 = BeautifulSoup(html.text,'lxml')
-    print(bs4.prettify())
+    trs = bs4.find_all('table')[3].find_all("tr")[1:]
+    for tr in trs:
+        try:
+            name = tr.find_all('td')[1].get_text().strip()
+            buy = tr.find_all('td')[2].find('font').get_text().strip()
+            sell = tr.find_all('td')[3].find('font').get_text().strip()
+            print(name,buy,sell)
+        except:
+            pass
