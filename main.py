8487ac1b18811d8554755ea5e65b5b9fe05a1342
DevHyung <khuphj@gmail.com>
2018-07-23 09:58:17 +0900

    BackUp file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #-*-encoding:utf8-*-
 from bs4 import BeautifulSoup
 import requests
-
+import xlsxwriter
 if __name__=="__main__":
     url = 'http://www.wooticket.com/popup_price.php'
     html = requests.get(url)
@@ -9,11 +9,29 @@ if __name__=="__main__":
     html.encoding = 'euc-kr'  # 한글 인코딩으로 변환
     bs4 = BeautifulSoup(html.text,'lxml')
     trs = bs4.find_all('table')[3].find_all("tr")[1:]
-    for tr in trs:
-        try:
-            name = tr.find_all('td')[1].get_text().strip()
-            buy = tr.find_all('td')[2].find('font').get_text().strip()
-            sell = tr.find_all('td')[3].find('font').get_text().strip()
-            print(name,buy,sell)
-        except:
-            pass
+    with xlsxwriter.Workbook('test.xlsx') as workbook:
+        # head
+        headList = []
+        titleList = []
+        buyList = []
+        worksheet = workbook.add_worksheet()
+        row = 2
+        col = 0
+        for tr in trs[1:-1]:
+            try:
+                name = tr.find_all('td')[1].get_text().strip()
+                titleList.append(name.split(' ', maxsplit=1)[1])
+                headList.append(name.split(' ',maxsplit=1)[0])
+            except:
+                headList.append('-')
+                titleList.append(name.split(' ', maxsplit=1)[0])
+            finally:
+                buy = tr.find_all('td')[2].find('font').get_text().strip()
+                buyList.append(buy)
+                sell = tr.find_all('td')[3].find('font').get_text().strip()
+        print(len(titleList))
+        print(len(headList))
+        worksheet.write_row(0, 0, headList)
+        worksheet.write_row(1, 0, titleList)
+        worksheet.write_row(2, 0, buyList)
+
